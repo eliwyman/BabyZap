519c581ace41e1db07bb9ae2095475d9203aa3cf
Elliot <eli@localhost.localdomain>
2014-11-14 17:04:22 -0800

    Substituded constants for the unnamed values in Sprite.java

--- Sprite.java
+++ Sprite.java
@@ -11,8 +11,10 @@ Each class instance contains one ship instance.
 	private final String PORT = "0";
 	private final long MAX;
 	private final long DMG;
-	private final float DMGMOD;
-	private final float HEALTHMOD;
+	private final double DMGMOD = 0.3;
+	private final double HEALTHMOD = 0.1;
+	private final long MIN_ENERGY = 20;
+	private final double FUEL_COST = 100;
 
 	private long energy;
 
@@ -21,8 +23,6 @@ Each class instance contains one ship instance.
 		energy = e;
 		MAX = m;
 		DMG = d;
-		DMGMOD = (float)(DMG/100f);
-		HEALTHMOD = (float)(10f/100f);
 	}
 
 	public String getShip(){
@@ -50,21 +50,21 @@ Each class instance contains one ship instance.
 	//REQUIRES: int dist, indicating distance between travel co-ordinates
 	//EFFECTS: returns true/false if the energy is greater than the fuel cost (distance * 100)
 
-		return(energy > dist*100);
+		return(energy > dist*FUEL_COST);
 	}
 
 	public void useFuel(double dist) {
 	//REQUIRES: int dist, indicating distance between travel co-ordinates
 	//MODIFIES: decrements the fuel cost from the ship's energy level
 
-		energy -= dist*100;
+		energy -= dist*FUEL_COST;
 	}
 
 
 	public boolean shipDead() {
 	//EFFECTS: returns true or false if the ship has died (energy drops below 20 units).
 	
-		return(energy < 20);
+		return(energy < MIN_ENERGY);
 	}
 
 	public boolean shipDead(int hits) {	 		
@@ -72,19 +72,18 @@ Each class instance contains one ship instance.
 	//MODIFIES: decrements the damage taken from the ship's energy
 	//EFFECTS: returns true or false if the ship has died (energy drops below 20 units).
 
-		float damage;
+		double damage;
 		for (int i = 0; i< hits; i++) {
 			damage = energy * DMGMOD;
 			energy -= damage;
 		}
-		return(energy < 20);
+		return(shipDead());
 	}
 
 	public void shipHeal() {
 	//MODIFIES: increases the ship's energy by the HEALTHMOD percentage.
 
-		float health = energy * HEALTHMOD;
-		energy += health;
+		energy += energy * HEALTHMOD;
 	}
 
 }
